d7a0c84b6e22330edb3fd05219bcb03901ba866c
eghbali.ehsan <eghbali.ehsan@sabaidea.team>
2025-01-06 15:22:59 +0330

    add utils

---
+++ utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+func GetHomeDir() string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("could not get home directory")
+	}
+	return currentDir
+}
